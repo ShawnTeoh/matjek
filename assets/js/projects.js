1aa3dbc0ef49e453fa4f9f91ce7060b0baa7d568
ShawnTeoh <thuan.teoh@uqconnect.edu.au>
2017-03-30 18:48:23 +0800

    Add switch to toggle forked projects

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -3,3 +3,11 @@ $(document).ready(function(){
     $(this).geopattern($(this).data('pattern-id'));
   });
 });
+
+function toggleForked(){
+  if ($('.fork-switch').prop('checked')) {
+    $('#forked').removeClass('hidden');
+  } else {
+    $('#forked').addClass('hidden');
+  }
+}
